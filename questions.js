8aefc2ab7ca8d35279e0c436081d4a2108dbb8ce
ViniMouraDev <101229941+ViniMouraDev@users.noreply.github.com>
2023-12-01 20:22:56 -0500

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,11 @@
+export default [
+    {
+        question: "Quanto é 1 + 2?",
+        answers: [
+            { option: "3", correct: true },
+            { option: "2", correct: false },
+            { option: "4", correct: false },
+        ],
+    },
+    
+]
